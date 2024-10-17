4b076f91b4ca67e0101f85e7e7a41e4b1cfca6a5
yashhsahu <146806394+yashhsahu@users.noreply.github.com>
2024-10-17 10:41:04 +0530

    Time: 9 ms (87.46%), Space: 19.6 MB (26.74%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int single = nums.front();
-        for(int val=1; val<nums.size(); val++){
-            single = single^nums.at(val);
+        int single = 0;
+        for(int val :  nums){
+            single = single^val;
         }
         return single;
     }
